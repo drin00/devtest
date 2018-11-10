5a976f73cc333ea92f71927f458298a231b8f3c0
Aldrin Padua <aldrinpadua@Aldrins-MacBook-Pro.local>
2018-11-11 07:45:49 +0800

    Add js file and create js for hoem and about

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function aboutFunction(){
+  document.getElementById("aboutus").style.display="block";
+  document.getElementById("banner").style.display="none";
+  document.getElementById("feedback").style.display="none";
+}
+
+function homeFunction(){
+  document.getElementById("aboutus").style.removeProperty('display');
+  document.getElementById("banner").style.removeProperty('display');
+  document.getElementById("feedback").style.removeProperty('display');
+}
