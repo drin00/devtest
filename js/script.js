8986fd1a250dcf005aba47b0b9151a68d0b617a8
Aldrin Padua <aldrinpadua@Aldrins-MacBook-Pro.local>
2018-11-11 13:43:23 +0800

    Change in banner animation and navbar behavior

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ function aboutFunction(){
   document.getElementById("aboutus").style="display:block;";
   document.getElementById("banner").style="display:none;";
   document.getElementById("feedback").style="display:none;";
+  window.scrollTo(0,0);
+
 }
 
 function homeFunction(){
+  window.scrollTo(0,0);
   document.getElementById("aboutus").style.removeProperty('display');
   document.getElementById("banner").style.removeProperty('display');
   document.getElementById("feedback").style.removeProperty('display');
+
 }
