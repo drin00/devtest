c9b0ead1060ff0ac2fdba2c2327d228941fa0a87
Aldrin Padua <aldrinpadua@Aldrins-MacBook-Pro.local>
2018-11-11 15:44:33 +0800

    Adjust auto-resize of navbar and removed bold font on body tag.

--- js/script.js
+++ js/script.js
@@ -17,11 +17,12 @@ function homeFunction(){
 window.onscroll = function() {shrinkNavBar()};
 
 function shrinkNavBar() {
-  if (document.body.scrollTop > 100  || document.documentElement.scrollTop > 5) {
-    document.getElementById("menubar").style.height = "5.5rem";
+  if (document.body.scrollTop > 53  || document.documentElement.scrollTop > 53) {
     document.getElementById("logo").style.width = "18rem";
+    document.getElementById("menubar").style = "height: 5.5rem; background-color: #F2F1F1;";
   } else {
     document.getElementById("menubar").style.removeProperty('height');
     document.getElementById("logo").style.removeProperty('width');
+    document.getElementById("menubar").style = "background-color: white;";
   }
 }
