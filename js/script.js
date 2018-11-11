c97d8c4720191e7ea8873a68640c4f65c81ee6c8
Aldrin Padua <aldrinpadua@Aldrins-MacBook-Pro.local>
2018-11-12 03:22:13 +0800

    Fix transitions

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function aboutFunction(){
   document.getElementById("aboutus").style="display:block;";
   document.getElementById("banner").style="display:none;";
   document.getElementById("feedback").style="display:none;";
+  document.getElementById("services").style="display:none;";
   window.scrollTo(0,0);
 
 }
@@ -11,18 +12,32 @@ function homeFunction(){
   document.getElementById("aboutus").style.removeProperty('display');
   document.getElementById("banner").style.removeProperty('display');
   document.getElementById("feedback").style.removeProperty('display');
+  document.getElementById("services").style.removeProperty('display');
+}
 
+function svcsFunction(){
+  document.getElementById("services").style="display:block;";
+  document.getElementById("banner").style="display:none;";
+  document.getElementById("feedback").style="display:none;";
+  document.getElementById("aboutus").style="display:none;";
+  window.scrollTo(0,0);
 }
 
 window.onscroll = function() {shrinkNavBar()};
 
 function shrinkNavBar() {
-  if (document.body.scrollTop > 53  || document.documentElement.scrollTop > 53) {
+  if (document.body.scrollTop > 10  || document.documentElement.scrollTop > 10) {
     document.getElementById("logo").style.width = "18rem";
-    document.getElementById("menubar").style = "height: 5.5rem; background-color: #F2F1F1;";
+    document.getElementById("menubar").style = "height: 5.5rem; background-color: rgba(242,241,241,0.7);";
   } else {
     document.getElementById("menubar").style.removeProperty('height');
     document.getElementById("logo").style.removeProperty('width');
     document.getElementById("menubar").style = "background-color: white;";
   }
 }
+
+
+function onHover() {
+  document.getElementById("ecomm").style = "background-color: gray;";
+}
+document.getElementById("ecomm").addEventListener("mouseover", onHover);
